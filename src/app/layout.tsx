953b5478d47c9315d38ab109ed960d941a684ea2
Mehedi Hasan <mehedi@etlimited.net>
2025-01-09 18:09:13 +0600

    refactor: update project configuration and improve error handling component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,29 +5,29 @@ import Header from "@/components/shared/Header";
 import Footer from "@/components/shared/Footer";
 
 const roboto = Roboto({
-  weight: "400",
-  subsets: ["latin"],
-  display: "swap",
+ weight: "400",
+ subsets: ["latin"],
+ display: "swap",
 });
 
 export const metadata: Metadata = {
-  title: "Blogiz",
-  description:
-    "Welcome to Blogiz – where innovation meets imagination in the dynamic realm of technology, offering a thrilling journey through the latest trends and groundbreaking discoveries in the world of tech!",
+ title: "Blogiz",
+ description:
+  "Welcome to Blogiz – where innovation meets imagination in the dynamic realm of technology, offering a thrilling journey through the latest trends and groundbreaking discoveries in the world of tech!",
 };
 
 export default function RootLayout({
-  children,
+ children,
 }: Readonly<{
-  children: React.ReactNode;
+ children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en" data-theme="light">
-      <body className={roboto.className}>
-        <Header />
-        <div className="min-h-screen">{children}</div>
-        <Footer />
-      </body>
-    </html>
-  );
+ return (
+  <html lang="en" data-theme="light">
+   <body className={roboto.className}>
+    <Header />
+    <div className="min-h-screen">{children}</div>
+    <Footer />
+   </body>
+  </html>
+ );
 }
